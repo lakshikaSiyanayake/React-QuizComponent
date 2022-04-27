c2566a070bb419eaafd35650225bfde84f65eaa6
lakshikaSiyanayake <lakshika.siyanayake@gmail.com>
2022-04-27 15:14:30 +1000

    add new component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import { render } from 'enzyme';
 import React, {Component} from 'react'
+import QuizQuestion from './QuizQuestion.js'
 
 let quizData = require('./quiz_data.json');
 
@@ -12,14 +13,10 @@ class Quiz extends Component
     }
     render(){
         return (
-        <div>
-            <div className='QuizQuestion'>
-                {quizData.quiz_questions[0].instruction_text}
-            </div>
-        </div>
+        <QuizQuestion {...quizData.quiz_question[-1]} />
         )
     }
-}
+} 
     
 
 
