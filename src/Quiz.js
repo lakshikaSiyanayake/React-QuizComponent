687bcb9604078e176443b19ae7242083bd416255
lakshikaSiyanayake <lakshika.siyanayake@gmail.com>
2022-04-28 10:50:06 +1000

    change code in quiz.js and quizquestion.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,20 @@
-import { render } from 'enzyme';
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
 
-class Quiz extends Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {quiz_position: 1}
-    }
-    render(){
-        return (
-            <div>
-              <QuizQuestion quiz_question= {quizData.quiz_question[this.state.quiz_position-1]} />
-            </div>
-        
-        )
-    }
-} 
-    
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  render() {
+    return (
+      <div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      </div>
+    )
+  }
+}
 
-
-export default Quiz;
+export default Quiz
