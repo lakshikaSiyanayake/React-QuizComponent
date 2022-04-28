4972f555ab02ffe64c68d480299e6567ea15819a
lakshikaSiyanayake <lakshika.siyanayake@gmail.com>
2022-04-28 15:23:43 +1000

    add button text

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -5,9 +5,7 @@ class QuizQuestionButton extends Component
     render(){
         return(
                 <li>
-                    <button>
-                    
-                    </button>
+                    <button>{this.props.button_text}</button>
                 </li>
             )
     }
